ee9b29540e7e9fd86cac9c86621bfb282d3c6460
Kjetil Valle <kjetil.valle@gmail.com>
2017-11-21 22:44:46 +0100

    Submit on enter in home page form

--- frontend/src/Page/Home.elm
+++ frontend/src/Page/Home.elm
@@ -15,10 +15,12 @@ import Material.Textfield as Textfield
 import Material.Options as Options
 import Material
 import Material.Button as Button
+import Json.Decode
+import Json.Decode
 
 
 type Msg
-    = EditField String
+    = Edit String
     | Submit
 
 
@@ -41,7 +43,8 @@ view model appState homeMsg mdlMsg =
             , Textfield.floatingLabel
             , Textfield.text_
             , Textfield.value model.username
-            , Options.onInput (homeMsg << EditField)
+            , Options.onInput (homeMsg << Edit)
+            , Options.on "keydown" (keyCodeDecoder keys.enter <| homeMsg Submit)
             ]
             []
         , Button.render mdlMsg
@@ -59,8 +62,41 @@ view model appState homeMsg mdlMsg =
 update : Msg -> AppState -> Model -> ( Model, Cmd Msg )
 update msg appState model =
     case msg of
-        EditField newName ->
+        Edit newName ->
             ( { model | username = newName }, Cmd.none )
 
         Submit ->
             ( model, Route.newUrl (User model.username) )
+
+
+keyCodeDecoder : Int -> msg -> Json.Decode.Decoder msg
+keyCodeDecoder keyCode msg =
+    let
+        checkKey actualCode =
+            if actualCode == keyCode then
+                Json.Decode.succeed msg
+            else
+                Json.Decode.fail "Wrong key"
+    in
+        Json.Decode.field "keyCode" Json.Decode.int
+            |> Json.Decode.andThen checkKey
+
+
+keys :
+    { arrowDown : Int
+    , arrowUp : Int
+    , enter : Int
+    , escape : Int
+    , questionMark : Int
+    , tab : Int
+    , a : Int
+    }
+keys =
+    { arrowDown = 40
+    , arrowUp = 38
+    , enter = 13
+    , questionMark = 191
+    , escape = 27
+    , tab = 8
+    , a = 65
+    }
