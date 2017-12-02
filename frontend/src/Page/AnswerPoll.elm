5675bd1ff06714f677792bba788cc5c6a52b752e
Kjetil Valle <kjetil.valle@gmail.com>
2017-12-02 22:15:35 +0100

    Display options on answer poll page

--- frontend/src/Page/AnswerPoll.elm
+++ frontend/src/Page/AnswerPoll.elm
@@ -10,16 +10,23 @@ module Page.AnswerPoll
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Data.Poll exposing (PollId, Poll)
+import Data.Game exposing (GameId, Game)
 import Material
+import Material.Options as Options
+import Material.Button as Button
 import Backend.Poll
 import Route
 import Data.AppState exposing (AppState)
 import Task exposing (Task)
 import Http
+import Utils exposing ((<<<))
+import Dict
+import Views.GameCard
 
 
 type Msg
     = GoToPoll PollId
+    | SetSelection GameId Bool
     | SubmitAnswer
 
 
@@ -35,9 +42,28 @@ init appState pollId =
 view : Model -> AppState -> (Msg -> msg) -> (Material.Msg msg -> msg) -> Html msg
 view model appState newPollMsg mdlMsg =
     div [ class "answer-poll-wrapper" ]
-        [ h3 [] [ text <| "Answer poll" ]
-        , text "asdf"
+        [ h3 [] [ text <| "What do you want to play?" ]
+        , submitButton (Dict.values model.games) (newPollMsg SubmitAnswer) mdlMsg appState.mdl
+        , div [ class "game-cards" ] <|
+            Views.GameCard.cards
+                (newPollMsg <<< SetSelection)
+                mdlMsg
+                appState.mdl
+                (Dict.values model.games)
         ]
+
+
+submitButton : List Game -> msg -> (Material.Msg msg -> msg) -> Material.Model -> Html msg
+submitButton games msg mdlMsg mdlModel =
+    Button.render mdlMsg
+        [ 0 ]
+        mdlModel
+        [ Button.raised
+        , Button.colored
+        , Button.disabled |> Options.when (List.all (not << .selected) games)
+        , Options.onClick msg
+        ]
+        [ text "Submit vote" ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -45,6 +71,16 @@ update msg model =
     case msg of
         GoToPoll pollId ->
             ( model, Route.newUrl <| Route.Poll pollId )
+
+        SetSelection gameId state ->
+            ( { model
+                | games =
+                    Dict.update gameId
+                        (Maybe.map (Data.Game.setSelection state))
+                        model.games
+              }
+            , Cmd.none
+            )
 
         SubmitAnswer ->
             -- TODO
