2eece6e15a9676b91c732161142f60d696778d33
Kjetil Valle <kjetil.valle@gmail.com>
2017-12-02 21:24:11 +0100

    Update API for working with polls

--- backend/app/firebase.py
+++ backend/app/firebase.py
@@ -8,20 +8,35 @@ def connect():
 	cred = credentials.Certificate('firebase-serviceAccountKey.json')
 	firebase_admin.initialize_app(cred, config.firebase)
 
+
 def get_game(game_id):
 	return db.reference('games').child(game_id).get()
+
 
 def save_game(game_id, game):
 	games_ref = db.reference('games')
 	games_ref.child(game_id).set(game)
 
+
 def save_games(games):
 	db.reference('games').set(games)
 
+
 def save_poll(game_ids):
-	polls_ref = db.reference('polls')
-	new_poll_ref = polls_ref.push(game_ids)
+	"""Create a new poll from list of game IDs"""
+
+	polls_ref = db.reference("polls")
+	new_poll_ref = polls_ref.push()
+	new_poll_ref.child("game_ids").set(game_ids)
+	new_poll_ref.child("votes").set([])
+
 	return new_poll_ref.key
+
+
+def vote_poll(poll_id, name, game_ids):
+	polls_ref = db.reference("polls")
+	polls_ref.child(poll_id).child("votes").child(name).set(game_ids)
+
 
 def get_poll(poll_id):
 	return db.reference('polls').child(poll_id).get()
@@ -29,9 +44,6 @@ def get_poll(poll_id):
 
 if __name__ == "__main__":
 	connect()
+	vote_poll("-L-NrWR2MFz9mrBkty9W", "tine", ["50"])
 
-	dummy_ref = db.reference('dummy')
-	new_ref = dummy_ref.push({"foo": "baz"})
-
-	print new_ref.key
-	print new_ref.get()
+	print get_poll("-L-NrWR2MFz9mrBkty9W")
--- backend/app/main.py
+++ backend/app/main.py
@@ -42,21 +42,26 @@ def get_user_games(username):
 @cross_origin(headers=['Content-Type', 'Accept'])
 def post_poll():
     game_ids = request.get_json()
-
     if game_ids is None:
         return bad_request()
 
     poll_id = firebase.save_poll(game_ids)
 
-    json_data = json.dumps({
-        "id": poll_id
-    })
+    json_data = json.dumps({ "id": poll_id })
     return Response(response=json_data, status=200, mimetype="application/json")
+
 
 @application.route("/poll/<poll_id>", methods=['GET'])
 @cross_origin(headers=['Content-Type', 'Accept'])
 def get_poll(poll_id):
-    poll = firebase.get_poll(poll_id)
+    fb_poll = firebase.get_poll(poll_id)
+
+    poll = {
+        "id": poll_id,
+        "games": { game_id: firebase.get_game(game_id)
+                    for game_id in fb_poll["game_ids"]},
+        "votes": fb_poll["votes"]
+    }
 
     json_data = json.dumps(poll)
     return Response(response=json_data, status=200, mimetype="application/json")
