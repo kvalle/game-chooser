9564c0bd7a51b9148ba325dd1d8c5bc4cde9b98c
Kjetil Valle <kjetil.valle@gmail.com>
2018-04-29 13:36:19 +0200

    Get user info from BGG

--- lambdas/fetch-collection/lambda.py
+++ lambdas/fetch-collection/lambda.py
@@ -1,8 +1,16 @@
-def handler(event, context):
-    message = 'val1 is {} and val2 is {}'.format(event['val1'],
-                                                 event['val2'])
+import requests
+import xmltodict
 
-    print("Dummy lambda says: " + message)
-    return {
-        'message' : message
-    }
+def get_user(name):
+	r = requests.get('https://www.boardgamegeek.com/xmlapi2/user?name=' + name)
+	data = xmltodict.parse(r.text)
+
+	return {
+		"id": data["user"]["@id"],
+		"username": name,
+		"firstname": data["user"]["firstname"]["@value"],
+		"lastname": data["user"]["lastname"]["@value"]
+	}
+def handler(event, context):
+    user = get_user(event['username'])
+    return user
