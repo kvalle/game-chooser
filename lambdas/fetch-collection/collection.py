08fe5ae851eb575045d4ff9191f5cf2821cb0fed
Kjetil Valle <kjetil.valle@gmail.com>
2018-04-29 21:10:14 +0200

    Merge collection module into main

--- lambdas/fetch-collection/collection.py
+++
@@ -1,8 +0,0 @@
-
-
-# Collection states
-
-STATE_IDLE = "IDLE"        # request for game list run, need to wait for BGG
-STATE_WAITING = "WAITING"  # request for games given enough time, needs checking
-STATE_LOADED = "LOADED"    # game list loaded successfully
-STATE_FAILED = "FAILED"    # failed to load game list
--- lambdas/fetch-collection/main.py
+++ lambdas/fetch-collection/main.py
@@ -2,7 +2,12 @@ from datetime import datetime
 
 import bgg
 import database
-import collection
+
+# Collection states
+STATE_IDLE = "IDLE"        # request for game list run, need to wait for BGG
+STATE_WAITING = "WAITING"  # request for games given enough time, needs checking
+STATE_LOADED = "LOADED"    # game list loaded successfully
+STATE_FAILED = "FAILED"    # failed to load game list
 
 def handler(event, context):
     username = event['username']
@@ -10,7 +15,7 @@ def handler(event, context):
     collection = bgg.get_user(username)
     collection["created"] = datetime.utcnow().isoformat()
     collection["updated"] = datetime.utcnow().isoformat()
-    collection["state"] = collection.STATE_WAITING
+    collection["state"] = STATE_WAITING
 
     database.store_collection(collection)
 
