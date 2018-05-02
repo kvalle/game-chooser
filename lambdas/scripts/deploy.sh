c97b34fa9415000c6eea3c49b4b014182f1307e5
Kjetil Valle <kjetil.valle@gmail.com>
2018-05-02 19:08:37 +0200

    Correct path to code bundle in deploy script

--- lambdas/scripts/deploy.sh
+++ lambdas/scripts/deploy.sh
@@ -16,6 +16,6 @@ echo "Deploying lambda"
 envchain aws-privat aws lambda update-function-code \
   --region eu-central-1 \
   --function-name arn:aws:lambda:eu-central-1:017978203355:function:game-chooser--${lambda_name} \
-  --zip-file fileb://build/package.zip
+  --zip-file fileb://../build/package.zip
 
 echo "Done"
