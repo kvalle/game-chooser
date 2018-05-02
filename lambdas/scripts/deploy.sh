d6c3e117811d8b0dc7f0f6641d1181d037aa79a9
Kjetil Valle <kjetil.valle@gmail.com>
2018-05-02 20:54:04 +0200

    Update deploy script to use cloudformation

--- lambdas/scripts/deploy.sh
+++ lambdas/scripts/deploy.sh
@@ -7,15 +7,19 @@ while [ -h "$BASEDIR/$0" ]; do
     SYM=$(readlink $BASEDIR/$0)
     BASEDIR=$(cd $DIR && cd $(dirname -- "$SYM") && pwd)
 done
-cd $BASEDIR
+cd $BASEDIR/..
 
 echo "Packaging code bundle"
-./build.sh
+./scripts/build.sh
 
 echo "Deploying lambda"
-envchain aws-privat aws lambda update-function-code \
-  --region eu-central-1 \
-  --function-name arn:aws:lambda:eu-central-1:017978203355:function:game-chooser--${lambda_name} \
-  --zip-file fileb://../build/package.zip
+envchain aws-privat aws cloudformation package \
+  --s3-bucket game-chooser--lambda-bucket \
+  --template-file template.yml \
+  --output-template-file build/packaged.yml
+
+envchain aws-privat aws cloudformation deploy \
+  --template-file build/packaged.yml \
+  --stack-name game-chooser
 
 echo "Done"
