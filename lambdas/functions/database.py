c9ee2d794885a5a463b7c99efd4f58e8daae53b4
Kjetil Valle <kjetil.valle@gmail.com>
2018-05-02 21:24:59 +0200

    Simplify database module

--- lambdas/functions/database.py
+++ lambdas/functions/database.py
@@ -4,12 +4,4 @@ dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
 
 def store_collection(collection):
     table = dynamodb.Table('game-chooser--collections')
-
-    table.put_item(
-       Item={
-            'username': collection['username'],
-            'last_name': collection['last_name'],
-            'first_name': collection['first_name'],
-            'bgg_user_id': collection['bgg_user_id']
-        }
-    )
+    table.put_item(Item=collection)
